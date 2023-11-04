b151df8561db09ded714e4203fab79f224154c5c
Nihad <nihadalizadaxx@gmail.com>
2023-11-04 23:39:15 +0400

    added tailwind and created route

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import SignIn from "./Pages/SignIn"
+import About from "./Pages/About"
+import Profile from "./Pages/Profile"
+import LogIn from "./Pages/LogIn"
+
+export default function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/log-in" element={<LogIn/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}

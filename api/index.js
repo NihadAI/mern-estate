57565cfa6bf7ac8cb78c6515c4068c5e8a3b5792
Nihad <nihadalizadaxx@gmail.com>
2023-11-05 08:43:59 +0400

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('connected to Mongo');
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('running on port 300');
-    }
+}
 );

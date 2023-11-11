01bdf089c7943af2b63e83f43db9ba31ff33a607
Nihad <nihadalizadaxx@gmail.com>
2023-11-11 22:04:46 +0400

    completed user listings, created get listing route

--- api/Controllers/user.controller.js
+++ api/Controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../Models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import Listing from "../Models/listing.model.js";
 
 export const test = ('/test', (req, res) => {
     res.json({
@@ -49,4 +50,15 @@ export const deleteUser = async (req, res, next) => {
     }
 };
   
-  
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./Pages/Profile"
 import SignUp from "./Pages/SignUp"
 import Header from "./Components/Header"
 import PrivateRoute from "./Components/PrivateRoute"
+import CreateListing from "./Pages/CreateListing"
 
 export default function App() {
 
@@ -20,6 +21,7 @@ export default function App() {
       <Route path="/about" element={<About/>} />
       <Route element={<PrivateRoute/>} >
         <Route path="/profile" element={<Profile/>} />
+        <Route path='/create-listing' element={<CreateListing />} />
       </Route>
     </Routes>
     </BrowserRouter>
